7e18447e21430347da0ac36e3c34b0013233720b
harry bournis <harrybournis@gmail.com>
2016-12-02 18:13:32 +0200

    added client, message, chat models in client

---
+++ CITYMumbler.Client/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using CITYMumbler.Networking.Sockets;
+
+namespace CITYMumbler.Client
+{
+	public class Client
+	{
+		public int ID { get; private set; }
+		public string Name { get; private set; }
+
+		public Client(int id, string name)
+		{
+			this.ID = id;
+			this.Name = name;
+		}
+	}
+}
---
+++ CITYMumbler.Client/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITYMumbler.Client
+{
+	public class Message
+	{
+
+		public ushort SenderId { get; private set; }
+		public ushort ReceiverId { get; private set; }
+		public string SenderName { get; private set; }
+		public string Content { get; private set; }
+
+
+		public Message(ushort senderId, ushort receiverId, string senderName, string message)
+		{
+			SenderId = senderId;
+			ReceiverId = receiverId;
+			SenderName = senderName;
+			this.Content = message;
+		}
+	}
+}
