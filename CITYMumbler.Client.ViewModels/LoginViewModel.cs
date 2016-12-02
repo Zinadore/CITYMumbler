106ee6c28b0cc1436e1ca5a66333406931bc6780
Ioannis Boutsikas <mgioanni@outlook.com>
2016-12-02 18:06:40 +0200

    Added logging to Client's login view

--- CITYMumbler.Client.ViewModels/LoginViewModel.cs
+++ CITYMumbler.Client.ViewModels/LoginViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Reactive;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -10,25 +11,56 @@ using System.Threading.Tasks;
 using CITYMumbler.Common.Contracts.Services.Logger;
 using ReactiveUI;
 using Splat;
+using ILogger = CITYMumbler.Common.Contracts.Services.Logger.ILogger;
+using LogLevel = CITYMumbler.Common.Contracts.Services.Logger.LogLevel;
 
 namespace CITYMumbler.Client.ViewModels
 {
     public class LoginViewModel : ReactiveObject, IRoutableViewModel
     {
+        #region Private Members
+        private MumblerClient _mumblerClient;
+        private int _portNumeric;
+        private IPAddress _addressIp;
+        private IObservable<bool> CanExecuteConnect;
+        private ILogger logger;
+        #endregion
+
+        #region IRoutableViewModel
         public string UrlPathSegment => "login";
         public IScreen HostScreen { get; }
-		public ReactiveCommand<Unit, Unit> ConnnectCommand;
+        #endregion
 
-		private MumblerClient _mumblerClient;
-		private int _portNumeric;
-		private IPAddress _addressIp;
+        #region Output Properties
+        private ObservableAsPropertyHelper<bool> _isConnectButtonEnabled;
+        public bool IsConnectButtonEnabled => _isConnectButtonEnabled.Value;
 
-		private string _address;
-	    public string Address
-	    {
-		    get { return _address; }
-		    set { this.RaiseAndSetIfChanged(ref _address, value); }
-	    } 
+        private readonly ObservableAsPropertyHelper<bool> _isAddressValid;
+        public bool IsAddressValid
+        {
+            get { return _isAddressValid.Value; }
+        }
+
+        private readonly ObservableAsPropertyHelper<bool> _isPortValid;
+        public bool IsPortValid
+        {
+            get { return _isPortValid.Value; }
+        }
+
+        private readonly ObservableAsPropertyHelper<bool> _isUsernameValid;
+        public bool IsUsernameValid
+        {
+            get { return _isUsernameValid.Value; }
+        }
+        #endregion
+
+        #region Reactive Properties
+        private string _address;
+        public string Address
+        {
+            get { return _address; }
+            set { this.RaiseAndSetIfChanged(ref _address, value); }
+        }
 
         private string _port;
         public string Port
@@ -43,40 +75,22 @@ namespace CITYMumbler.Client.ViewModels
             get { return _username; }
             set { this.RaiseAndSetIfChanged(ref _username, value); }
         }
+        #endregion
 
-        private readonly ObservableAsPropertyHelper<string> _error;
-        public string Error
-        {
-            get { return "error default"; }
-        }
+        public ReactiveCommand<Unit, Unit> ConnnectCommand;
 
-	    private ObservableAsPropertyHelper<bool> _isConnectButtonEnabled;
-        public bool IsConnectButtonEnabled => _isConnectButtonEnabled.Value;
-
-	    private readonly ObservableAsPropertyHelper<bool> _isAddressValid;
-	    public bool IsAddressValid
-	    {
-		    get { return _isAddressValid.Value; }
-	    }
-
-	    private readonly ObservableAsPropertyHelper<bool> _isPortValid;
-	    public bool IsPortValid
-	    {
-		    get { return _isPortValid.Value; }
-	    }
-
-	    private readonly ObservableAsPropertyHelper<bool> _isUsernameValid;
-	    public bool IsUsernameValid
-	    {
-		    get { return _isUsernameValid.Value; }
-	    }
+        public IObservable<LogEntry> Logs { get; private set; }
+	    
 
 		public LoginViewModel(IScreen host)
         {
 			this.Address  = "127.0.0.1";
 			this.Port = "21992";
-			HostScreen = host;
-			_mumblerClient = Locator.Current.GetService<MumblerClient>();
+			this.HostScreen = host;
+			this._mumblerClient = Locator.Current.GetService<MumblerClient>();
+		    this.logger = Locator.Current.GetService<ILoggerService>().GetLogger(this.GetType());
+
+		    this.Logs = Locator.Current.GetService<ILoggerService>().Entries;
 
 			this.WhenAnyValue(x => x.Address)
 			    .Select(x => x?.Trim())
@@ -96,39 +110,23 @@ namespace CITYMumbler.Client.ViewModels
 			    .Select(x => !string.IsNullOrEmpty(x))
 			    .ToProperty(this, @this => @this.IsUsernameValid, out _isUsernameValid, false);
 
-			var CanExecuteConnect = this.WhenAnyValue(x => x.IsAddressValid, x => x.IsPortValid, x => x.IsUsernameValid,
+			 CanExecuteConnect = this.WhenAnyValue(x => x.IsAddressValid, x => x.IsPortValid, x => x.IsUsernameValid,
 				(address, port, username) => address && port && username);
 
 			CanExecuteConnect.ToProperty(this, @this => @this.IsConnectButtonEnabled, out _isConnectButtonEnabled);
-			//this._canConnect = this.WhenAnyValue(x => x.Address, x => x.Port, x => x.Username,
-			//    (address, port, username) => !string.IsNullOrWhiteSpace(address) && !string.IsNullOrWhiteSpace(port) &&
-			//                                 !string.IsNullOrWhiteSpace(username) && int.TryParse(Port, out _portNumberic) && IPAddress.TryParse(address, out _addressIp))
-			//    .ToProperty(this, @this => @this.CanConnect, false);
 
 			this.ConnnectCommand = ReactiveCommand.Create(Connect, CanExecuteConnect);
-
-			//this.WhenAnyValue(x => x.Address, x => x.Port, x => x.Username,
-			//    (address, port, username) => new Credentials(address, port, username));
-		}
+		    this.ConnnectCommand.ThrownExceptions.Subscribe(ex =>
+		    {
+		        this.logger.Log(LogLevel.Error, ex.Message);
+		    });
+        }
 
 	    private void Connect()
 	    {
 			_mumblerClient.Connect(_addressIp, _portNumeric, Username);
 		}
-    }
 
-    struct Credentials
-    {
-        public string Address { get; private set; }
-        public int Port { get; private set; }
-        public string Username { get; private set; }
-
-        public Credentials(string address, string port, string username)
-        {
-            Address = address;
-            Port = Int32.Parse(port);
-            Username = username;
-        }
 
     }
 }
